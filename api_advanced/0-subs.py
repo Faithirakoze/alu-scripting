9150d454be4407da1a8627782cc025505d38dd07
Faithirakoze <f.irakoze2@alustudent.com>
2025-07-02 11:23:05 -0700

    Task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
 """
-This script queries the Reddit API and returns the number of subscribers
-for a given subreddit.
+Reddit Subscriber Counter
+
+This script defines a function to query the Reddit API and return
+the number of subscribers for a given subreddit.
+
+If the subreddit is invalid or cannot be accessed, it returns 0.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a valid subreddit,
-      otherwise returns 0"""
+   """
+    Queries the Reddit API and returns the number of subscribers
+    for the specified subreddit.
+
+    Args:
+        subreddit (str): The subreddit name (e.g., "python").
+
+    Returns:
+        int: The number of subscribers, or 0 if the subreddit is invalid.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "0-subs.py/0.1"}
 
